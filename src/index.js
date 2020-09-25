ec6322633e4b9fc77e4017b0e4611181b07b0d43
CMarah <carlos@walcu.com>
2020-09-26 01:50:04 +0200

    First commit, basic functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+import { faSync } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { downloadData, getResults } from './poe_worth.js';
+
+const App = () => {
+  const [league, setLeague] = useState('Heist');
+  const [min_budget, setMinBudget] = useState('');
+  const [max_budget, setMaxBudget] = useState('');
+  const [data, setData] = useState(null);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (!data || league !== data.league) downloadData(league).then(data => setData(data));
+  }, [data, league]);
+  useEffect(() => {
+    setResults(data ? getResults(data, min_budget, max_budget, league) : []);
+  }, [data, min_budget, max_budget, league]);
+  const changeLeague = event => {
+    setData(null);
+    setLeague(event.target.value);
+  };
+
+  return (
+    <div className="App" style={{'textAlign': 'left'}}>
+      <div className="menu" style={{'paddingLeft': '4em'}}>
+        <div className="budget" style={{position: 'relative', marginRight: '2em'}}>
+          <div className="budget-text">Budget</div>
+          <input className="budget-input" placeholder="From" type="Number"
+            value={min_budget} onChange={event => setMinBudget(event.target.value)}
+          />
+          <input className="budget-input" placeholder="To" type="Number"
+            value={max_budget} onChange={event => setMaxBudget(event.target.value)}
+          />
+        </div>
+        <div className="league-selector">
+          <select value={league} onChange={changeLeague}>
+            <option value="Heist">Heist</option>
+            <option value="Hardcore Heist">HC Heist</option>
+            <option value="Standard">Standard</option>
+            <option value="Hardcore">Hardcore</option>
+          </select>
+        </div>
+        <div onClick={() => setData(null)} style={{margin: '0 4em 0 auto'}}>
+          <FontAwesomeIcon icon={faSync} size="2x" />
+        </div>
+      </div>
+      <table style={{maxWidth: '900px', margin: '1.5em auto'}} className="table-striped">
+        <thead style={{textAlign: 'center'}}>
+          <tr>
+            <th><span>Name</span></th>
+            <th><span>Item</span></th>
+            <th><span>Stack</span></th>
+            <th><span>Total</span></th>
+            <th style={{maxWidth: '60px'}}><span>Item Price</span></th>
+            <th style={{maxWidth: '60px'}}><span>Benefit</span></th>
+            <th><span>BPC</span></th>
+          </tr>
+        </thead>
+        <tbody>{results.map((c, i) =>
+          <tr key={i} style={{cursor: 'pointer'}}
+            onClick={() => window.open(c[8], '_blank')}
+          >
+            <td>
+              <img src={require("./images/card.png")} style={{
+                height: '32px',
+                verticalAlign: 'middle',
+                marginRight: '4px',
+              }}/>
+              {c[0]}
+            </td>
+            <td>{c[1]}</td>
+            <td style={{widtd: '5em'}}>{c[2] + ' x ' + c[3]}</td>
+            <td style={{widtd: '5em'}}>{c[4]}</td>
+            <td style={{widtd: '5em'}}>{c[5]}</td>
+            <td style={{widtd: '5em'}}>{c[6]}</td>
+            <td style={{widtd: '5em'}}>{c[7]}</td>
+          </tr>
+        )}</tbody>
+      </table>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App/>,
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/poe_worth.js
@@ -0,0 +1,128 @@
+const fetch = require('isomorphic-fetch');
+
+const ITEMS_TO_HIDE = ["Precursor's Emblem", "Soul Taker"];
+const EXCEPTION_CARDS = ['Wealth and Power', "The Dragon's Heart"];
+
+module.exports.downloadData = async league => {
+  const fetchPoeNinja = type => fetch(
+    'https://cors-anywhere.herokuapp.com/https://poe.ninja/api/data/' +
+    `${type === 'Currency' ? 'currencyoverview' : 'itemoverview'}?league=${league}&type=${type}`
+  )
+    .then(res => res.json())
+    .then(res => res.lines);
+  const downloads = await Promise.all([
+    fetchPoeNinja('DivinationCard'),
+    fetchPoeNinja('UniqueWeapon'),
+    fetchPoeNinja('UniqueArmour'),
+    fetchPoeNinja('UniqueAccessory'),
+    fetchPoeNinja('Prophecy'),
+    fetchPoeNinja('UniqueFlask'),
+    fetchPoeNinja('SkillGem'),
+    fetchPoeNinja('Currency'),
+  ]);
+
+  const keys = [
+    'card_info', 'weapon_info', 'armour_info', 'acc_info',
+    'proph_info', 'flask_info', 'gem_info', 'curr_info',
+  ];
+  return downloads.reduce((acc, dl, i) => ({
+    ...acc,
+    [keys[i]]: dl,
+  }), { league });
+};
+
+const getTradeUrl = (name, price, league) =>
+  `https://www.pathofexile.com/trade/search/${league}?q={` +
+  "%22query%22:{%22filters%22:{%22type_filters%22:{%22filters%22:{%22category" +
+  `%22:{%22option%22:%22card%22}}}},%22type%22:%22${name.replace(' ', '%20')}%22}}`;
+
+const getCardsMap = card_info => card_info
+.filter(card => EXCEPTION_CARDS.includes(card.name) || (
+  card.chaosValue > 3 &&
+  card.explicitModifiers.length && (
+    card.explicitModifiers[0].text.includes('uniqueitem') ||
+    card.explicitModifiers[0].text.includes('divination') ||
+    card.explicitModifiers[0].text.includes('currencyitem') ||
+    card.explicitModifiers[0].text.includes('prophecy')
+  ) &&
+  card.explicitModifiers[0].text.match(/{['\s\w]+}/)
+)).map(card => ({
+  name: card.name,
+  stack_size: card.stackSize,
+  single_price: card.chaosValue,
+  total_price: parseFloat((card.chaosValue*card.stackSize).toFixed(0)),
+  item: card.explicitModifiers[0].text.match(/{[\w'\s]+}/)[0].slice(1,-1),
+  type: card.explicitModifiers[0].text.match(/\w+/)[0],
+  raw_item: card.explicitModifiers,
+}));
+
+const getCurrencyItem = (item, { curr_info }) => {
+  const currency = curr_info.find(c => item.includes(c.currencyTypeName));
+  const amount = item.match(/\d+/) || 1;
+  if (!currency || !amount || !currency.receive) return null;
+  return {
+    chaosValue: amount*currency.receive.value,
+  };
+}
+
+const getItem = (item, type, info) => {
+  if (false && item === 'Level 4 Enlighten') return {
+    chaosValue: info.gem_info.find(
+      g => g.name === 'Enlighten Support' && g.gemLevel === 4
+    ).chaosValue,
+  };
+  if (false && item === 'Level 4 Empower') return {
+    chaosValue: info.gem_info.find(
+      g => g.name === 'Empower Support' && g.gemLevel === 4
+    ).chaosValue,
+  };
+
+  const { card_info, weapon_info, armour_info, acc_info, proph_info, flask_info } = info;
+  const gear = [...weapon_info, ...armour_info, ...acc_info, ...flask_info].find(
+    g => g.name === item && g.links === 0
+  );
+  const card_o = card_info.find(c => c.name === item && c.links === 0);
+  const prophecy = proph_info.find(p => p.name.includes(item));
+  const currency = type === 'currencyitem' ? getCurrencyItem(item, info) : null;
+  return gear ||card_o || prophecy || currency;
+}
+
+const myNumber = num => parseFloat(num.toFixed(0)).toLocaleString();
+
+module.exports.getResults = (data, min_budget, max_budget, league) => {
+  const clean_cards = getCardsMap(data.card_info);
+
+  const trades = clean_cards.map(card => {
+    const item = getItem(card.item, card.type, data);
+    return !item ? null : {
+      ...card,
+      item_price: item.chaosValue,
+      benefit: item.chaosValue - card.total_price,
+      proportional_benefit: (item.chaosValue - card.total_price)/card.total_price,
+      benefit_per_card: (item.chaosValue - card.total_price)/card.stack_size,
+      trade_url: getTradeUrl(card.name, card.single_price, league),
+    };
+  })
+    .filter(c => c
+      && c.total_price > (min_budget || 0)
+      && c.total_price < (max_budget || Infinity)
+      && !ITEMS_TO_HIDE.includes(c.item)
+    );
+  const best_trades = trades.sort((a,b) => a.benefit_per_card - b.benefit_per_card);
+  return best_trades.reverse().map(tr => ([
+    tr.name.slice(0,24),
+    tr.item,
+    tr.stack_size,
+    ...([
+      tr.single_price,
+      tr.total_price,
+      tr.item_price,
+      tr.benefit,
+      tr.benefit_per_card,
+    ].map(x => myNumber(x))),
+    tr.trade_url,
+  ]));
+};
+
+//TODO the bargain
+//TODO the white knight
